8aa8a7f91d63dd097576ee94b0e639f85f380f3c
Riku <rikukestila@gmail.com>
2025-02-22 08:11:34 +0200

    Display questions

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,28 @@
+import { use, useState } from "react";
+import QUESTIONS from "../questions";
+export const Quiz = () => {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  const handleSelectedAnswer = (selectedAnswer) => {
+    setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+  };
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+            <li key={index} className="answer">
+              <button onClick={() => handleSelectedAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
