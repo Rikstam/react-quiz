b600df5a5281c6f04c9d08efaad1c1cbc62a7b02
Riku <rikukestila@gmail.com>
2025-03-02 09:47:15 +0200

    Clean up timeouts and intervals

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,13 +4,15 @@ export const QuestionTimer = ({ timeout, onTimeout }) => {
   const [remainingTime, setRemainingTime] = useState(timeout);
   
   useEffect(() => {
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+    return () => clearTimeout(timer);
   }, [onTimeout, timeout]);
 
   useEffect(() => {
-    setInterval(() => {
+    const interval =  setInterval(() => {
       setRemainingTime((prevTime) => prevTime - 100);
     }, 100);
+    return () => clearInterval(interval);
   }, []);
   return <progress id="question-time" value={remainingTime} max={timeout} />;
 };
